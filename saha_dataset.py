cc1e299d17356e732a9b1fe014d8685be3ab4fe0
aarahman <arif@themascoteers.com>
2022-07-05 16:38:59 +1000

    README.md

--- saha_dataset.py
+++ saha_dataset.py
@@ -3,70 +3,62 @@ import pandas as pd
 from sklearn import preprocessing
 from pandas.api.types import is_string_dtype, is_numeric_dtype
 import torch
+from sklearn import model_selection
 
 
 class SahaDataset(Dataset):
     def __init__(self, is_train):
-        self.TRAIN_RATIO = 2
-        self.NAN_TOLERANCE = 0.5
         self.is_train = is_train
         self.file_location = "saha.csv"
         csv_data = pd.read_csv("saha.csv")
         self.total = len(csv_data)
-
         df = pd.DataFrame(csv_data)
-        df = self._normalize(df)
+        df = self._preprocess(df)
 
+        # Dumping for debug purpose
         df.to_csv("out.csv")
 
-        self.test_count = len(df) // self.TRAIN_RATIO
-        if len(df) % self.TRAIN_RATIO != 0:
-            self.test_count += 1
-        self.train_count = len(df) - self.test_count
-        self.count = self.train_count
-        if self.is_train is False:
-            self.count = self.test_count
+        train, test = model_selection.train_test_split(df, test_size=0.2)
+        df = train
+        if not self.is_train:
+            df = test
 
-        self.input_map = list()
-        self.x_dim = 0
-        self.y_dim = 0
-        for index, row in df.iterrows():
-            last_cell = row[len(row)-1]
-            self.y_dim = self._length(last_cell)
-            for i in range(len(row)-1):
+        self.x_tensor, self.y_tensor = self._tensorise(df)
+        self.x_dim = self.x_tensor.shape[1]
+
+    def _calculate_dim(self, x):
+        dim = 0
+        for index, row in x.iterrows():
+            for i in range(len(row)):
                 cell = row[i]
                 length = self._length(cell)
-                self.x_dim = self.x_dim + length
-                if length == 1:
-                    self.input_map.append(df.columns[i])
-                else:
-                    for counter in range(length):
-                        self.input_map.append(df.columns[i]+str(counter))
+                dim = dim + length
             break
+        return dim
 
-        self.samples = torch.zeros((self.count, self.x_dim))
-        self.targets = torch.zeros(self.count, dtype=torch.float32)
+    def _tensorise(self, df):
+        x = df.iloc[:, :-1]
+        x_dim = self._calculate_dim(x)
+        last_column = df.iloc[: , -1]
+        count = len(df)
+        tensor_x = torch.zeros((count, x_dim))
 
-        current_index = 0
-        for index, row in df.iterrows():
-            mod = index % self.TRAIN_RATIO
-            if is_train and mod == 0:
-                continue
-            if is_train is False and mod != 0:
-                continue
-            last_cell = row[len(row)-1]
-            self.targets[current_index] = last_cell
+        row_index = 0
+        for index, row in x.iterrows():
             start_index = 0
             end_index = 0
-            for i in range(len(row)-1):
+            for i in range(len(row)):
                 cell = row[i]
                 if torch.is_tensor(cell):
                     end_index = start_index + cell.shape[0]
                 else:
                     end_index = start_index + 1
-                self.samples[current_index, start_index:end_index] = cell
+                tensor_x[row_index, start_index:end_index] = cell
                 start_index = end_index
-            current_index = current_index + 1
+            row_index = row_index + 1
+        last_column = list(last_column)
+        tensor_y = torch.Tensor(last_column)
+        return tensor_x, tensor_y
 
     def _length(self, var):
         if torch.is_tensor(var):
@@ -75,12 +67,12 @@ class SahaDataset(Dataset):
             return 1
 
     def __len__(self):
-        return self.targets.shape[0]
+        return self.x_tensor.shape[0]
 
     def __getitem__(self, idx):
-        return self.samples[idx], self.targets[idx]
+        return self.x_tensor[idx], self.y_tensor[idx]
 
-    def _normalize(self, df):
+    def _preprocess(self, df):
         for col in df.columns:
             if is_numeric_dtype(df[col]):
                 df = self._normalize_numeric(df, col)
